5250cbf33733369559a3e6c9fd972b0b71fa7336
Nils Haberkamp <haberkamp.n@gmail.com>
2025-12-22 21:29:30 +0100

    fix: make it easier to undestand playback direction of sprite

--- src/components/atoms/SpriteAnimationWithControls.tsx
+++ src/components/atoms/SpriteAnimationWithControls.tsx
@@ -13,9 +13,15 @@ const PauseIcon = () => (
   </svg>
 );
 
-const ReverseIcon = () => (
+const ForwardIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <path fill="currentColor" d="M16 2h-2v2h2v2H4v2H2v5h2V8h12v2h-2v2h2v-2h2V8h2V6h-2V4h-2zM6 20h2v2h2v-2H8v-2h12v-2h2v-5h-2v5H8v-2h2v-2H8v2H6v2H4v2h2z" />
+    <path fill="currentColor" d="M18 4v16h2V4zM4 11v2h8v2h-2v2h2v-2h2v-2h2v-2h-2V9h-2V7h-2v2h2v2z" />
+  </svg>
+);
+
+const BackwardIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M6 4v16H4V4zm14 7v2h-8v2h-2v-2H8v-2h2V9h2v2zm-8-2V7h2v2zm0 6h2v2h-2z" />
   </svg>
 );
 
@@ -70,8 +76,8 @@ export default function SpriteAnimationWithControls({
           className={buttonClass}
           onClick={() => setIsReversed((r) => !r)}
         >
-          <ReverseIcon />
-          {isReversed ? 'Forward' : 'Reverse'}
+          {isReversed ? <BackwardIcon /> : <ForwardIcon />}
+          {isReversed ? 'Reverse' : 'Forward'}
         </button>
       </div>
     </div>
